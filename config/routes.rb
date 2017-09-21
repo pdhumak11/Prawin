342a1da60375813fb892c938fa3e8a3489d5d927
Pravin Prakash  Dhumak <pdhumak.11@gmail.com>
2017-09-21 15:48:58 +0530

    chane routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  	match '/contacts',     to: 'contacts#new',             via: 'get'
+  	#match '/contacts',     to: 'contacts#new',             via: 'get'
 		resources "contacts", only: [:new, :create]
+		root :to => "contacts#new"
 end
